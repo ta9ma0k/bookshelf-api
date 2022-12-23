26d443025b99d33f26e0ab188cda30e37e05337f
okada takuma <takuma@pop-os.localdomain>
2022-12-23 14:44:11 +0900

    refactoring

--- src/main/kotlin/com/atw/bookshelfapi/controller/uniquebooks/UniqueBooksController.kt
+++ src/main/kotlin/com/atw/bookshelfapi/controller/uniquebooks/UniqueBooksController.kt
@@ -1,21 +1,22 @@
 package com.atw.bookshelfapi.controller.uniquebooks
 
 import com.atw.bookshelfapi.usecase.query.uniquebooks.UniqueBooksQuery
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 data class BookResponseDto(
-    val isbn: String,
-    val title: String,
-    val imgSrc: String?
+  val isbn: String,
+  val title: String,
+  val imgSrc: String?
 )
 
 @RestController
 class UniqueBooksController(
-    private val uniqueBooksQuery: UniqueBooksQuery
+  private val uniqueBooksQuery: UniqueBooksQuery
 ) {
-  @GetMapping("/unique-books")
-  fun index(): List<BookResponseDto> {
-    return uniqueBooksQuery.getAll().map { BookResponseDto(it.isbn, it.title, it.imgSrc) }
+  @GetMapping("unique-books")
+  fun index(): ResponseEntity<List<BookResponseDto>> {
+    return ResponseEntity.ok(uniqueBooksQuery.getAll().map { BookResponseDto(it.isbn, it.title, it.imgSrc) })
   }
 }
