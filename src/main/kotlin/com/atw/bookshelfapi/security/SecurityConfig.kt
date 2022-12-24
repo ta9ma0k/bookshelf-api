38056a0400bfa0fdfeae3d8c9504915966b9df85
okada takuma <takuma@pop-os.localdomain>
2022-12-24 21:48:11 +0900

    setting cors

--- src/main/kotlin/com/atw/bookshelfapi/security/SecurityConfig.kt
+++ src/main/kotlin/com/atw/bookshelfapi/security/SecurityConfig.kt
@@ -3,14 +3,17 @@ package com.atw.bookshelfapi.security
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +26,7 @@ class WebSecurityConfig {
     objectMapper: ObjectMapper,
   ): SecurityFilterChain =
     http
-      .cors {}
+      .cors { cors -> cors.configurationSource(corsConfigurationSource())}
       .authorizeHttpRequests { auth ->
         auth
           .requestMatchers("/login").permitAll()
@@ -40,4 +43,17 @@ class WebSecurityConfig {
   @Bean
   fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager =
     authenticationConfiguration.authenticationManager
+
+  private fun corsConfigurationSource(): CorsConfigurationSource {
+    val configuration = CorsConfiguration()
+    configuration.allowedOrigins = listOf("http://localhost:5173")
+    configuration.allowedHeaders = listOf(CorsConfiguration.ALL)
+    configuration.allowedMethods = listOf("GET", "POST", "OPTIONS")
+    configuration.allowCredentials = true
+    configuration.exposedHeaders = listOf(HttpHeaders.AUTHORIZATION)
+
+    val source = UrlBasedCorsConfigurationSource()
+    source.registerCorsConfiguration("/**", configuration)
+    return source
+  }
 }
